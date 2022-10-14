f2336c6b62debbac582ebfbe855e82f9c1cd2059
Tom <tomwansww@gmail.com>
2022-10-14 11:10:19 +0500

    Create basic ui on app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'game-apollon';
+export class AppComponent implements OnInit {
+  @ViewChild('canvasGame') canvasGame!: ElementRef<HTMLCanvasElement>;
+  fuelAmount = 0;
+  velocity = 0;
+
+  ngOnInit(): void {}
+
+  newGame(): void {}
+
+  turnOnEngine(): void {}
+
+  turnOffEngine(): void {}
 }
